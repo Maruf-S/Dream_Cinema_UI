76a108c44a02f0dd9f75fb3173a5671573b6bd16
Taklemariam Alazar <teklealazar399@gmail.com>
2021-06-18 04:15:39 +0300

    update made on home and about

--- assets/js/home.js
+++ assets/js/home.js
@@ -29,16 +29,173 @@ $('.carousel_slider').carousel({
 // end
 
 // api calls for top movies
-function getMovies() {
+function getTopMovies() {
   fetch(`${baseUrl}/api/v1/movies`, {
   method: "GET",
-  body: JSON.stringify(_data),
+  headers: {"Content-type": "application/json; charset=UTF-8"
+          }
+  })
+  .then(response => response.json())
+        .then(function(data){
+      // *****************************************************************
+      // data.forEach(function(movie) {
+      //   if(movie["IDMBRating"] > 5){
+        // <div class="carousel-item active ">
+      //               <div class="row">
+      //                   <div class="col-lg-3 col-md-6 col-sm-12 topShowCard">
+      //                       <div class="card topShowCard">
+      //                           <img class="img-fluid topShowsImage" alt="100%x100%"
+      //                               src="{movie["Postor"]}">
+      //                           <div class="card-body">
+      //                               <h4 class="card-title">{movie["Title"]}</h4>
+      //                               <a class="btn topShowBtn" href="cinema.html?id={movie["id"]}" target="_blank">Get Ticket</a>
+
+      //                           </div>
+
+      //                       </div>
+      //                   </div>
+      //                   <div class="col-lg-3 col-md-6 col-sm-12 topShowCard">
+      //                       <div class="card topShowCard">
+      //                           <img class="img-fluid topShowsImage" alt="100%x100%" src="{movie["Postor"]}">
+      //                           <div class="card-body">
+      //                               <h4 class="card-title">{movie["Title"]}</h4>
+      //                               <a class="btn topShowBtn" href="cinema.html?id=112" target="_blank">Get Ticket</a>
+
+      //                           </div>
+      //                       </div>
+      //                   </div>
+      //                   <div class="col-lg-3 col-md-6 col-sm-12 topShowCard">
+      //                       <div class="card">
+      //                           <img class="img-fluid topShowsImage" alt="100%x100%"
+      //                               src="{movie["Postor"]}">
+      //                           <div class="card-body">
+      //                               <h4 class="card-title">{movie["Title"]}</h4>
+      //                               <a class="btn topShowBtn" href="cinema.html?id={movie["id"]}" target="_blank">Get Ticket</a>
+
+      //                           </div>
+      //                       </div>
+      //                   </div>
+      //                   <div class="col-lg-3 col-md-6 col-sm-12 topShowCard">
+      //                       <div class="card">
+      //                           <img class="img-fluid topShowsImage" alt="100%x100%" src="{movie["Postor"]}">
+      //                           <div class="card-body">
+      //                               <h4 class="card-title">{movie["Title"]}</h4>
+      //                               <a class="btn topShowBtn" href="cinema.html?id={movie["id"]}" target="_blank">Get Ticket</a>
+
+      //                           </div>
+      //                       </div>
+      //                   </div>
+
+
+      //               </div>
+      //           </div>
+      // }
+      // }
+      
+      
+      // *****************************************************************
+      console.log(data)
+
+  }) 
+  .catch(err => {
+      console.log(err);
+      // return `Unknown error occurred${error}`;
+  });
+}
+//#endregion
+
+// Carousel window interval
+// $('.innerCarousel').carousel({
+//     interval: 1000
+// });
+
+// api calls for new movies
+function getNewMovies() {
+  fetch(`${baseUrl}/api/v1/movies`, {
+  method: "GET",
   headers: {"Content-type": "application/json; charset=UTF-8"
           }
   })
   .then(response => response.json())
         .then(function(data){
       // work goes here
+      // *****************************************************************
+      // for(int i; i<3;i++){
+        // data.forEach(function(movie) {
+
+          //   if(i==1 && movie["Genre"] = "Action"){
+            // <div class="tabPanel">
+        //                   <!-- New releases -->
+        //                   <p></p>
+        //                   <div class="row">
+        //                       <div class="col-lg-3 col-md-6 col-sm-12">
+        //                           <div class="card hovereffect">
+        //                               <img class="card-img-top"
+        //                                   data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+        //                                   alt="Thumbnail [100%x225]" src="{movie["Postor"]}"
+        //                                   data-holder-rendered="true">
+        //                               <div class="overlay">
+        //                                   <a class="info" href="cinema.html?id={movie["id"]}">Get Ticket</a>
+        //                               </div>
+        //                               <div class="card-body">
+        //                                   <h4 class="card-title">{movie["Title"]}</h4>
+        //                               </div>
+        //                           </div>
+        //                       </div>
+      
+        //                       <div class="col-lg-3 col-md-6 col-sm-12">
+        //                           <div class="card hovereffect">
+        //                               <img class="card-img-top"
+        //                                   data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+        //                                   alt="Thumbnail [100%x225]" src="{movie["Postor"]}"
+        //                                   data-holder-rendered="true">
+        //                               <div class="overlay">
+        //                                   <a class="info" href="cinema.html?id={movie["id"]}">Get Ticket</a>
+        //                               </div>
+        //                               <div class="card-body">
+        //                                   <h4 class="card-title">{movie["Title"]}</h4>
+        //                               </div>
+        //                           </div>
+        //                       </div>
+      
+        //                       <div class="col-lg-3 col-md-6 col-sm-12">
+        //                           <div class="card hovereffect">
+        //                               <img class="card-img-top"
+        //                                   data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+        //                                   alt="Thumbnail [100%x225]" src="{movie["Postor"]}"
+        //                                   data-holder-rendered="true">
+        //                               <div class="overlay">
+        //                                   <a class="info" href="cinema.html?id={movie["id"]}">Get Ticket</a>
+        //                               </div>
+        //                               <div class="card-body">
+        //                                   <h4 class="card-title">{movie["Title"]}</h4>
+        //                               </div>
+        //                           </div>
+        //                       </div>
+      
+        //                       <div class="col-lg-3 col-md-6 col-sm-12">
+        //                           <div class="card hovereffect">
+        //                               <img class="card-img-top"
+        //                                   data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
+        //                                   alt="Thumbnail [100%x225]" src="{movie["Postor"]}" data-holder-rendered="true">
+        //                               <div class="overlay">
+        //                                   <a class="info" href="cinema.html?id={movie["id"]}">Get Ticket</a>
+        //                               </div>
+        //                               <div class="card-body">
+        //                                   <h4 class="card-title">{movie["Title"]}</h4>
+        //                               </div>
+        //                           </div>
+        //                       </div>
+      
+        //                   </div>
+      
+        //               </div>
+        // }
+      //   }
+
+      // }
+      
+      // *****************************************************************
       console.log(data)
 
   }) 
